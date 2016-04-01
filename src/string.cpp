a96965beab6d5aeffca0618343fec9fd6fb23b9b
mephisto94 <mephisto94@yandex.ru>
2016-04-01 12:07:26 +0300

    change

--- src/string.cpp
+++ src/string.cpp
@@ -56,7 +56,7 @@ String::String(String &&other) {
 
 
 String::~String() {
-    if (string[0] != null_terminator & length != 0) {
+    if (string[0] != null_terminator ) {
         delete[] string;
     }
 }
